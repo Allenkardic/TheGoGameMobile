0025f4d401a56586a0a56a7a316f79aa16849bab
“aliakwe” <aliakwe@yahoo.com>
2024-12-10 19:16:39 -0500

    clean up

--- app/components/EmptyList/index.tsx
+++ app/components/EmptyList/index.tsx
@@ -1,4 +1,4 @@
-import {memo} from 'react';
+import React, {memo} from 'react';
 import {Image, StyleSheet, View} from 'react-native';
 import {Images, Spacing} from '../../utils';
 import Text from '../Typography';
